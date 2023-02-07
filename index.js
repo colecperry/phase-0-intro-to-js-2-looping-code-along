20d33f2505501152620d5cb838e0569cc49f271f
Cole Perry <colecperry@gmail.com>
2023-02-07 15:51:19 -0800

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const cards = ["Charlie", "Samip", "Ali"]
+const event = ("Birthday")
+const arrayName = []
+
+function writeCards(cards, event) {
+  for (let i = 0; i < cards.length; i++) {
+    arrayName.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return arrayName;
+}
+
+//writeCards(cards,event);
+
+function countDown(count) {
+    while (count >= 0) {
+    console.log(count);
+    count-=1}
+    //console.log(count)
+}
